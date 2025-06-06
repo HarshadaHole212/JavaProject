8a5ce79f7a533db5cbf2c6a808b67199dac25084
HarshadaHole212 <harshadahole212@gmail.com>
2025-06-06 14:23:10 +0530

    1 line deleted

--- src/ex_01_Java_Basic.java
+++ src/ex_01_Java_Basic.java
@@ -2,6 +2,5 @@ public class ex_01_Java_Basic {
     public static void main(String[] args){
         System.out.println("Lets learn java");
         System.out.println(2+2);
-        System.out.println("Commit done");
     }
 }
