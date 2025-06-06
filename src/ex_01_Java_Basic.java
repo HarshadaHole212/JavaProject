57c5bedfcbf6700133bc712de12a24b2fee807d1
HarshadaHole212 <harshadahole212@gmail.com>
2025-06-06 12:32:53 +0530

    new changes

--- src/ex_01_Java_Basic.java
+++ src/ex_01_Java_Basic.java
@@ -2,6 +2,6 @@ public class ex_01_Java_Basic {
     public static void main(String[] args){
         System.out.println("Lets learn java");
         System.out.println(2+2);
-        System.out.println("Commit");
+        System.out.println("Commit done");
     }
 }
