b745b17b1c99a6980a360e1ccf4edb64dd164aea
HarshadaHole212 <harshadahole212@gmail.com>
2025-07-01 16:00:06 +0530

    Operators

---
+++ src/Operators.java
@@ -0,0 +1,83 @@
+public class Operators {
+/*
+    1. Arithmetic Operators
+    Used to perform basic mathematical operations.
+
+    Operator	Description	Example
++	Addition	a + b
+-	Subtraction	a - b
+*	Multiplication	a * b
+/	Division	a / b
+%	Modulus (remainder)	a % b
+
+2. Relational (Comparison) Operators
+    Used to compare two values.
+
+    Operator	Description	Example
+==	Equal to	a == b
+!=	Not equal to	a != b
+>	Greater than	a > b
+    <	Less than	a < b
+            >=	Greater or equal	a >= b
+<=	Less or equal	a <= b
+
+3. Logical Operators
+    Used to combine multiple boolean expressions.
+
+    Operator	Description	Example
+&&	Logical AND	a > 5 && b < 10
+            `		`
+            !	Logical NOT	!true
+
+            4. Assignment Operators
+    Used to assign values to variables.
+
+    Operator	Description	Example
+=	Assign	a = 10
+            +=	Add and assign	a += 5 (means a = a + 5)
+            -=	Subtract and assign	a -= 5
+            *=	Multiply and assign	a *= 2
+            /=	Divide and assign	a /= 2
+            %=	Modulus and assign	a %= 2
+
+            5. Unary Operators
+    Work with a single operand.
+
+    Operator	Description	Example
++	Unary plus	+a
+-	Unary minus	-a
+++	Increment	a++ or ++a
+--	Decrement	a-- or --a
+!	Logical NOT	!flag
+
+6. Bitwise Operators
+    Operate on bits (rarely used by beginners).
+
+    Operator	Description	Example
+&	Bitwise AND	a & b
+`	`	Bitwise OR
+^	Bitwise XOR	a ^ b
+~	Bitwise Complement	~a
+<<	Left shift	a << 2
+            >>	Right shift	a >> 2
+
+            7. Ternary Operator
+    Short form of if-else.
+
+    java
+            Copy
+    Edit
+    int max = (a > b) ? a : b;
+    Syntax: condition ? value_if_true : value_if_false
+
+8. Instanceof Operator
+    Checks if an object is of a specific class type.
+
+    java
+            Copy
+    Edit
+if (obj instanceof String) {
+        // true if obj is a String
+    }
+*/
+}
