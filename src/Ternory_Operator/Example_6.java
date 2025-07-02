59e6d3618f884af7448e585f01472b35720115bb
HarshadaHole212 <harshadahole212@gmail.com>
2025-07-02 17:11:51 +0530

    Ternory Operator more examples

---
+++ src/Ternory_Operator/Example_6.java
@@ -0,0 +1,9 @@
+package Ternory_Operator;
+
+public class Example_6 {
+    public static void main(String[] args) {
+        int marks = 30;
+        String m = (marks>=40)? " Pass " : " Fail ";
+        System.out.println(m);
+    }
+}
---
+++ src/Ternory_Operator/Example_8.java
@@ -0,0 +1,9 @@
+package Ternory_Operator;
+
+public class Example_8 {
+    public static void main(String[] args) {
+        int num = -9;
+        String n = (num>0)? num+" Absolute" : num+" not absolute";
+        System.out.println(n);
+    }
+}
